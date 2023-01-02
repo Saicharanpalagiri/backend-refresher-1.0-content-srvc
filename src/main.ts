21f12b5dfec63635a69acd5bd2c90269fb90f4d6
Saicharanpalagiri <charan517214@gmail.com>
2023-01-02 12:48:39 +0530

    content

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        'amqps://ghdswtyl:0tdkAKnW71Ty8Pe3JVvDtd3qI43pr2fX@puffin.rmq2.cloudamqp.com/ghdswtyl',
+      ],
+      queue: 'CONTENT_SRVC_QUEUE',
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+  await app.startAllMicroservices();
+  await app.listen(5000);
 }
 bootstrap();
